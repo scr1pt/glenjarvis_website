3189c57fdc09020d2b40d5fe89cbc23d38bfd332
Glen Jarvis <glen@glenjarvis.com>
2013-06-20 19:44:02 -0700

    Add Twitter Bootstrap; Recommendation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 GlenjarvisCom::Application.routes.draw do
-  resources :homes
-
+  resources :recommendations
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +50,7 @@ GlenjarvisCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'recommendations#index'
 
   # See how all your routes lay out with "rake routes"
 
