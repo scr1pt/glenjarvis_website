d9e3fb81c1aa747b22359d55845ca230407e85bf
Glen Jarvis <glen@glenjarvis.com>
2013-08-28 23:34:34 -0700

    Add Rails 4.0 scaffolding (rails new)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GlenJarvisCom::Application.config.secret_key_base = '60aa1645b747692ae291ba8b88c8487c6e7b640db7842fdb037c257010dd2a56f84c24b54430f782f238ec0774d7ce291a7e827eb5505b7b26c91953d6a02316'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GlenJarvisCom::Application.config.session_store :cookie_store, key: '_GlenJarvisCom_session'
