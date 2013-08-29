34ee538f55202c2a6ad759b9822052ecdba78403
Glen Jarvis <glen@glenjarvis.com>
2013-08-29 00:00:13 -0700

    Add secret_token for secure when archive is public

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,24 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# This code from Michael Hartl's excellent rails tutorial:
+# http://ruby.railstutorial.org/chapters/static-pages#code-secret_token
+
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
+require 'securerandom'
 
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-GlenJarvisCom::Application.config.secret_key_base = '60aa1645b747692ae291ba8b88c8487c6e7b640db7842fdb037c257010dd2a56f84c24b54430f782f238ec0774d7ce291a7e827eb5505b7b26c91953d6a02316'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+GlenJarvisCom::Application.config.secret_key_base = secure_token
