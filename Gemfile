ec1d40e7b489fd982a4cc327421eb8fcfd3a6f0a
Glen Jarvis <glen@glenjarvis.com>
2014-07-04 11:58:25 -0700

    Initial draft of capistrano deploy

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
-#ruby-gemset=glenjarvis-website
+#ruby-gemset glenjarvis-website
 
 gem 'zip', '2.0.2'
 gem 'rails', '4.0.0'
@@ -30,7 +30,6 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,12 @@ set :repository,  "git@github.com:glenjarvis/glenjarvis-website.git"
 set :scm, :git
 set :use_sudo, false
 set :deploy_to, "/var/apps"
-set :user, "web"
+set :user, "webuser"
 
-role :web, "aws_prod1"
-role :app, "aws_prod1"
-role :db,  "aws_prod1", :primary => true
+ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/pem/glenjarvis_llc/aws_glenjarvis_prod.pem"]
+
+role :web, "54.86.89.18"
+role :app, "54.86.89.18"
+role :db,  "54.86.89.18", :primary => true
 
 set :rvm_ruby_string, 'ruby-2.0.0-p481@glenjarvis-website'
